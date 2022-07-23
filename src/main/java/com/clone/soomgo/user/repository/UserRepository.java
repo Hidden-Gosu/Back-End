b436d9a1409ce5e6bd0c3961b010a89c80d06a71
kim-ju-ho <kim1jo2ho3@naver.com>
2022-07-23 23:35:39 +0900

    fix. userRepository

--- src/main/java/com/clone/soomgo/user/repository/UserRepository.java
+++ src/main/java/com/clone/soomgo/user/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.clone.soomgo.user.repository;
 import com.clone.soomgo.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
